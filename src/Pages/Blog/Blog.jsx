d2f2e98a4e906db593363933413db2972e8308ff
omarjafor <omarjaforchy@gmail.com>
2024-01-05 21:30:00 +0600

    conditional rendering

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -4,8 +4,23 @@ const Blog = () => {
     return (
         <div>
             Blog Page
+            <ConditionalRander></ConditionalRander>
         </div>
     );
 };
 
 export default Blog;
+
+
+
+const ConditionalRander = ({ showContent }) => {
+    return (
+        <div>
+            {showContent ? (
+                <p>Content is visible.</p>
+            ) : (
+                <p>Content is hidden.</p>
+            )}
+        </div>
+    );
+};
