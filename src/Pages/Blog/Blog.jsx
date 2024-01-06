6f8d9d56b369fff0fe7e4ae22d09024b8c44f76e
omarjafor <omarjaforchy@gmail.com>
2024-01-06 11:15:50 +0600

    some update

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -4,23 +4,8 @@ const Blog = () => {
     return (
         <div>
             Blog Page
-            <ConditionalRander></ConditionalRander>
         </div>
     );
 };
 
 export default Blog;
-
-
-
-const ConditionalRander = ({ showContent }) => {
-    return (
-        <div>
-            {showContent ? (
-                <p>Content is visible.</p>
-            ) : (
-                <p>Content is hidden.</p>
-            )}
-        </div>
-    );
-};
